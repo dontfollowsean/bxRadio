b3679b652408aba419c6e9f5cc2836e7613fc6b1
Sean Wilkinson <sean.wilkinson@bouncex.com>
2019-09-13 23:46:49 -0400

    display artist names and links

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,39 @@
 const nowplaying = document.querySelector(".now-playing"),
-  recentlyPlayedDiv = document.querySelector(".recently-played-container"),
-  toggle = document.querySelector("#toggle"),
-  nowPlayingEndpoint = "https://bxradio.seanw.io/nowplaying",
-  recentlyPlayedEndpoint = "https://bxradio.seanw.io/recentlyplayed";
+    recentlyPlayedDiv = document.querySelector(".recently-played-container"),
+    toggle = document.querySelector("#toggle"),
+    nowPlayingEndpoint = "https://bxradio.seanw.io/nowplaying",
+    recentlyPlayedEndpoint = "https://bxradio.seanw.io/recentlyplayed";
+
+function getArtistText(artist) {
+    let text = "";
+    for (let i = 0; i < artist.length; i++) {
+        if (i > 0 && artist.length > 2) {
+            text += ", ";
+        }
+        if (i > 0 && i === artist.length - 1) {
+            text += "and ";
+        }
+        text += `<span onclick="window.open('${artist[i].url}', '_blank')">` + artist[i].name + `</span>`
+        ;
+    }
+
+    return text
+}
 
 const renderNowPlaying = song => {
-  let image = song.images.find(i => i.width === 640);
+    let image = song.images.find(i => i.width === 640);
 
-  if (image === null) {
-    image = {};
-    image.url = "./public/images/generic-image.png";
-  }
+    if (image === null) {
+        image = {};
+        image.url = "./public/images/generic-image.png";
+    }
 
-  nowplaying.innerHTML = `
-    <div class="now-playing-container" onclick="window.open('${song.url}', '_blank')">
+    nowplaying.innerHTML = `
+    <div class="now-playing-container">
       <label>Now Playing</label>
       <div class="now-playing-info">
-        <div class="now-playing-title">${song.title}</div>
-        <div class="now-playing-artist">${song.artist}</div>
+        <div class="now-playing-title" onclick="window.open('${song.url}', '_blank')">${song.title}</div>
+        <div class="now-playing-artist">${getArtistText(song.artist)}</div>
       </div>
     </div>
     <div class="now-playing-img" onclick="window.open('${song.url}', '_blank')">
@@ -25,22 +41,22 @@ const renderNowPlaying = song => {
     </div>
   `;
 
-  setTimeout(fetchNowPlaying, song.duration - song.progress);
+    setTimeout(fetchNowPlaying, song.duration - song.progress);
 };
 
 const renderRecentlyPlayed = songs => {
-  let songInfoHtml = "";
+    let songInfoHtml = "";
 
-  songs.forEach(song => {
-    let image = song.images.find(i => i.width === 64);
+    songs.forEach(song => {
+        let image = song.images.find(i => i.width === 64);
 
-    if (image === null) {
-      image = {};
-      image.url = "./public/images/generic-image.png";
-    }
+        if (image === null) {
+            image = {};
+            image.url = "./public/images/generic-image.png";
+        }
 
-    songInfoHtml += `
-        <div class="single-song-container" 
+        songInfoHtml += `
+      <div class="single-song-container" 
           onclick="window.open('${song.url}', '_blank')">
           <img
             src=${image.url}
@@ -49,21 +65,21 @@ const renderRecentlyPlayed = songs => {
           />
           <div class="single-song-content">
             <div class="single-song-title">${song.title}</div>
-            <div class="single-song-artist">${song.artist}</div>
+            <div class="single-song-artist">${getArtistText(song.artist)}</div>
           </div>
       </div>
     `;
-  });
+    });
 
-  recentlyPlayedDiv.innerHTML = songInfoHtml;
+    recentlyPlayedDiv.innerHTML = songInfoHtml;
 };
 
-/* 
-  TODO - 
+/*
+  TODO -
     Proper error handling
 */
 const displayNotPlaying = () => {
-  nowplaying.innerHTML = `
+    nowplaying.innerHTML = `
     <div class="now-playing-container">
       <div class="now-playing-info">
         <div class="now-playing-title">No music is playing</div>
@@ -76,62 +92,62 @@ const displayNotPlaying = () => {
 };
 
 const fetchNowPlaying = () => {
-  fetch(nowPlayingEndpoint)
-    .then(response => {
-      if (response.status === 204){
-        displayNotPlaying();
-        return
-      }
-     return response.json()
-    })
-    .then((song) => {
-      setTimeout(fetchRecentlyPlayed, (song.duration - song.progress) + 5000);
-      renderNowPlaying(song);
-    })
-    .catch(err => console.log(err));
+    fetch(nowPlayingEndpoint)
+        .then(response => {
+            if (response.status === 204) {
+                displayNotPlaying();
+                return
+            }
+            return response.json()
+        })
+        .then((song) => {
+            setTimeout(fetchRecentlyPlayed, (song.duration - song.progress) + 5000);
+            renderNowPlaying(song);
+        })
+        .catch(err => console.log(err));
 };
 
 const fetchRecentlyPlayed = () => {
-  fetch(recentlyPlayedEndpoint)
-    .then(response => response.json())
-    .then(renderRecentlyPlayed)
-    .catch(err => console.log(err));
+    fetch(recentlyPlayedEndpoint)
+        .then(response => response.json())
+        .then(renderRecentlyPlayed)
+        .catch(err => console.log(err));
 };
 
 const displaySongs = () => {
-  fetchNowPlaying();
-  fetchRecentlyPlayed()
+    fetchNowPlaying();
+    fetchRecentlyPlayed()
 };
 
 const styles = {
-  blue: {
-    "--theme-background-color":
-      "linear-gradient(0deg, #0047c8 0%, #0080d4 75%)",
-    "--theme-text-color": "#f8e71c",
-    "--theme-single-hover-background-color": "#0266c1",
-    "--theme-single-background-color": "#0047C8",
-    "--theme-single-shadow": "0px 2px 8px 2px rgba(0, 0, 0, 0.1)"
-  },
-  dark: {
-    "--theme-background-color":
-      "linear-gradient(0deg,rgba(0, 0, 0, 1) 0%, rgba(57, 57, 67, 1) 75%)",
-    "--theme-text-color": "#0085ff",
-    "--theme-single-hover-background-color": "#111112",
-    "--theme-single-background-color": "#18181d",
-    "--theme-single-shadow": "0px 2px 12px 2px rgba(0, 0, 0, 0.4)"
-  }
+    blue: {
+        "--theme-background-color":
+            "linear-gradient(0deg, #0047c8 0%, #0080d4 75%)",
+        "--theme-text-color": "#f8e71c",
+        "--theme-single-hover-background-color": "#0266c1",
+        "--theme-single-background-color": "#0047C8",
+        "--theme-single-shadow": "0px 2px 8px 2px rgba(0, 0, 0, 0.1)"
+    },
+    dark: {
+        "--theme-background-color":
+            "linear-gradient(0deg,rgba(0, 0, 0, 1) 0%, rgba(57, 57, 67, 1) 75%)",
+        "--theme-text-color": "#0085ff",
+        "--theme-single-hover-background-color": "#111112",
+        "--theme-single-background-color": "#18181d",
+        "--theme-single-shadow": "0px 2px 12px 2px rgba(0, 0, 0, 0.4)"
+    }
 };
 
 const toggleTheme = e => {
-  if (e.target.checked) {
-    Object.keys(styles.blue).map(key => {
-      document.documentElement.style.setProperty(key, styles.blue[key]);
-    });
-  } else {
-    Object.keys(styles.dark).map(key => {
-      document.documentElement.style.setProperty(key, styles.dark[key]);
-    });
-  }
+    if (e.target.checked) {
+        Object.keys(styles.blue).map(key => {
+            document.documentElement.style.setProperty(key, styles.blue[key]);
+        });
+    } else {
+        Object.keys(styles.dark).map(key => {
+            document.documentElement.style.setProperty(key, styles.dark[key]);
+        });
+    }
 };
 
 displaySongs();
